d2cf4c9030fb7ce8b76795b441f4d83298e80c9b
Krzysiek Partyka <krzyspartyka@gmail.com>
2023-09-20 12:00:33 +0200

    Servis Changes

--- Lunchroom.Application/Services/LunchroomService.cs
+++ Lunchroom.Application/Services/LunchroomService.cs
@@ -25,17 +25,6 @@ namespace Lunchroom.Application.Services
             _lunchroomRepository = lunchroomRepository;
             _mapper = mapper;
         }
-        public async Task Create(LunchroomDto lunchroomDto)
-        {
-            
-
-        }
-
-        public async Task<IEnumerable<LunchroomDto>> GetAll()
-        {
-            var lunchrooms = await _lunchroomRepository.GetAll();
-            var dto = _mapper.Map<IEnumerable<LunchroomDto>>(lunchrooms);
-            return dto;
-        }
+        
     }
 }
